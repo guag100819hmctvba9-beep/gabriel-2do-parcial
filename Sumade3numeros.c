8b61a412ab14a61cd8924f6526d8b9cfb90fd4d5
guag100819hmctvba9-beep <guag100819hmctvba9@lake.edu.mx>
2025-10-21 13:02:59 -0600

    Add files via upload

---
+++ Sumade3numeros.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main(){
+float numero1;
+float numero2;
+float numero3;
+float suma;
+
+printf("ingresa el numero\n");
+scanf("%f",numero1 );
+
+printf("ingresa el numero dos\n");
+scanf("%f",numero2);
+
+printf("ingresa el numero tres ");
+scanf("%f,numero3");
+suma=numero1 + numero2 + numero3;
+
+printf("la suma de los 3 numeros es :.2f",suma);
+}
